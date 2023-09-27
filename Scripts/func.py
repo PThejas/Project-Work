53a19ddb3c968f6ab330f8847bc17c824c4cda48
PThejas <144049561+PThejas@users.noreply.github.com>
2023-09-27 11:29:35 -0400

    Add files via upload

---
+++ Scripts/func.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def show_employee(ename, sal):
+    if sal > 10000:
+        grade = 'Manager'
+    else:
+        grade = 'Consultant'
+    print("Employee Name : " + ename)
+    print(f"Employee Name :{ename}")
+    print('Salary : ' + str(sal))
+    print("Grade : " + grade)
+
+show_employee('ABC',1000)
+
